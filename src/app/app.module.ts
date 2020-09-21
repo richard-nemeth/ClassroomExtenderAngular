697ec3350b2e80188cabd0d37678d6d2c0e8fb81
Németh Richárd <richard.nemeth@maxolutions.hu>
2020-09-21 20:05:55 +0200

    - Created a custom module for angular material elements. - Added it to the login.component.html.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {HttpClientModule} from '@angular/common/http';
 
+import {AngularMaterialModule} from './modules/angular-material.module';
+
 import {AppComponent} from './app.component';
 
 import {HeaderComponent} from './components/header/header.component';
 import {LoginComponent} from './components/login/login.component';
 
 import {RootService} from './services/root.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -20,6 +23,8 @@ import {RootService} from './services/root.service';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
   ],
   providers: [
     RootService
