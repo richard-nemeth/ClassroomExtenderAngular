34df03a3e5f58c397c33462bec43f8f76367c08f
Németh Richárd <richard.nemeth@maxolutions.hu>
2020-09-25 21:33:10 +0200

    - Outsourced host to a constant, easier maintenance.

--- src/app/constants/route.constants.ts
+++ src/app/constants/route.constants.ts
@@ -2,7 +2,9 @@ export class RouteConstants {
   private constructor() {
   }
 
+  private static readonly HOST: string = 'http://localhost:3000/';
+
   public static readonly Auth = class {
-    public static readonly Authentication: string = 'http://localhost:3000/authentication';
+    public static readonly Authentication: string = RouteConstants.HOST + 'authentication';
   }
 }
