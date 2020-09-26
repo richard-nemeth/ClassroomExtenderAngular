308ebe173691be3ae1da09a202426a59a9ca926a
Németh Richárd <richard.nemeth@maxolutions.hu>
2020-09-26 08:52:56 +0200

    - Navigation logid added, on success login to the guarded path.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,19 +1,24 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router';
 
 import {BackendEndpointConstants} from '../constants/backend-endpoint.constants';
+import { RouteConstants } from '../constants/route.constants';
+
 
 @Injectable()
 export class AuthService {
 
-  public constructor(private httpClient: HttpClient) {
+  public constructor(
+    private httpClient: HttpClient,
+    private router: Router
+  ) {
   }
 
   public async authenticate(): Promise<void> {
-
     await this.httpClient.get(BackendEndpointConstants.Auth.Authentication).toPromise().then(response => {
       if(response) {
-        console.log(response);
+        this.router.navigate([RouteConstants.GUARDER]);
       }
     });
   }
