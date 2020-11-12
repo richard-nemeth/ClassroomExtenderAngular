bdbd50563fd45125dfc521e07c4541e2c057b8fd
Németh Richárd <richard.nemeth@maxolutions.hu>
2020-11-12 21:08:37 +0100

    - Added minor theming for tabs.

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-content',
-  templateUrl: './content.component.html'
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
 })
 export class ContentComponent {
   
