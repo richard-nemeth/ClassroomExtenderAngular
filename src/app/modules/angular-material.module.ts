f4575a8a89319f85d7955fd1cd3c4229d56095e1
Németh Richárd <richard.nemeth@maxolutions.hu>
2020-11-14 23:35:33 +0100

    - Added details holder with proper styling.

--- src/app/modules/angular-material.module.ts
+++ src/app/modules/angular-material.module.ts
@@ -13,7 +13,6 @@ import {MatTableModule} from '@angular/material/table';
 import {MatPaginatorModule} from '@angular/material/paginator';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from "@angular/material/form-field";
-
 @NgModule({
   exports: [
     MatToolbarModule,
