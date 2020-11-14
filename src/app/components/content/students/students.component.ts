21ec6f683490b967327df04f054bdfbef3060b0c
Németh Richárd <richard.nemeth@maxolutions.hu>
2020-11-14 21:53:33 +0100

    - Fixed some loading logic.

--- src/app/components/content/students/students.component.ts
+++ src/app/components/content/students/students.component.ts
@@ -1,4 +1,6 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 import {Student} from 'src/app/models/courses/Student';
 
@@ -8,19 +10,29 @@ import {StudentsService} from 'src/app/services/students.service';
   selector: 'app-students',
   templateUrl: './students.component.html'
 })
-export class StudentsComponent implements OnInit {
+export class StudentsComponent implements OnInit, AfterViewInit {
 
   @Input() courseId: string;
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
   public readonly columns: string[] = ['name'];
-  public students: Student[];
-  public studentCount: number;
+  public dataSource = new MatTableDataSource<Student>();
+  public isDataloaded: boolean;
 
   public constructor(private studentsService: StudentsService) {
   }
 
   async ngOnInit(): Promise<void> {
-    this.students = await this.studentsService.getStudentsForCourse(this.courseId);
-    this.studentCount = this.students.length;
+    const students: Student[] = await this.studentsService.getStudentsForCourse(this.courseId);
+    
+    if (students) {
+      this.dataSource.data = students;
+      this.isDataloaded = true;
+    }
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
   }
 }
